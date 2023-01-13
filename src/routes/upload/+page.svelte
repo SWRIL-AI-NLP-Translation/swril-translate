b0bb76a91517c0d72d0d638a09c73bcee62a8245
Zac Waite <129032699zw@gmail.com>
2023-01-12 22:35:35 -0500

    Redirect on document upload

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -22,6 +22,7 @@
 		if (!file) return alert("File not found")
 		const res = await uploadFile(file, data.token??'') as {filename: string};
 		alert("Saved " + res.filename)
+		goto('/documents')
 	}
 </script>
 
